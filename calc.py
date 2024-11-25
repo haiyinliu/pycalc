50c6fb9498c6b450cc893e75f8d9ede6b8a673e7
Haiyin Liu <plasmoduck+github@gmail.com>
2024-11-25 11:46:41 +1100

    added multiplication operator

--- calc.py
+++ calc.py
@@ -1,8 +1,12 @@
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
-    if operator == '-':
+    elif operator == "*":
+        return num0 * num1
+    elif operator == '-':
         return num0 - num1
     else:
         print('unknown operator!')
