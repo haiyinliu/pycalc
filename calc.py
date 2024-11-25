ac5d55b81d8e5e0fbd2fca10662bf9166e58eadd
Haiyin Liu <plasmoduck+github@gmail.com>
2024-11-25 11:49:57 +1100

    add division

--- calc.py
+++ calc.py
@@ -2,8 +2,10 @@ def compute(expression):
     num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
-    if operator == '-':
+    elif operator == '-':
         return num0 - num1
+    elif operator == '/':
+        return num0 / num1
     else:
         print('unknown operator!')
         return None
